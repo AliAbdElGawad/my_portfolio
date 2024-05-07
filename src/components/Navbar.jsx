ff18c4db6102db93a3d000d46893593b3c7639c5
AliAbdElGawad <ali1531999@gmail.com>
2024-05-07 15:01:38 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import logo from "../assets/kevinRushLogo.png";
+import {
+  FaLinkedin,
+  FaTwitterSquare,
+  FaGithub,
+  FaInstagram,
+} from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between py-6 mb-20">
+      <div className="flex items-center flex-shrink-0">
+        <img className="w-10 mx-2" src={logo} alt="logo" />
+      </div>
+      <div className="flex items-center justify-center gap-4 m-8 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaTwitterSquare />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
